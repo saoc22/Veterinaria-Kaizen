f2555115c5d96077790a6fd1a4b599a2ac1891df
Unbe <saoc.2201@gmail.com>
2024-06-11 13:51:16 -0600

    Se elimino las librerias no utilizadas. Ninguna funcionalidad nueva fue agregada.

--- demo_principal.py
+++ demo_principal.py
@@ -1,9 +1,7 @@
 #GUI imports
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtGui
 #function imports
 from demo_GUI import pantalla_formatos, grid
 
